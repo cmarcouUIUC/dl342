e226edf297a5188848831764feb9c5156f3d541f
cmarcouUIUC <cmarcou@uillinois.edu>
2024-02-19 18:35:24 +0000

    train progress

--- homework1/homework/train.py
+++ homework1/homework/train.py
@@ -24,17 +24,17 @@ def train(args):
     valid_data=load_data('data/valid')
 
     #split into data and labels
-    for i, data in enumerate(train_data):
-        train_inputs, train_labels = data
+    #for i, data in enumerate(train_data):
+    #    train_inputs, train_labels = data
 
-    for i, data in enumerate(valid_data):
-        valid_inputs, valid_labels = data
+    #for i, data in enumerate(valid_data):
+    #    valid_inputs, valid_labels = data
 
     #Put data on device
-    train_inputs=train_inputs.to(device)
-    train_labels=train_labels.to(device)
-    valid_inputs=valid_inputs.to(device)
-    valid_labels=valid_labels.to(device)
+    #train_inputs=train_inputs.to(device)
+    #train_labels=train_labels.to(device)
+    #valid_inputs=valid_inputs.to(device)
+    #valid_labels=valid_labels.to(device)
 
     #create the model
     if args.load==True:
@@ -53,38 +53,24 @@ def train(args):
     train_accuracy = []
 
     for epoch in range(n_epochs):
-      permutation = torch.randperm(train_inputs.size(0))
-
-      #Iterate
-      for it in range(0,len(permutation)-batch_size+1, batch_size):
-        batch_samples = permutation[it:it+batch_size]
-        batch_data= train_inputs[batch_samples]
-        batch_label= train_labels[batch_samples]
-
-        #Compute Loss
-        o = model(batch_data)
-        loss_val = loss(o, batch_label.int())
-        
-        train_logger.add_scalar('train/loss', loss_val, global_step=global_step)
-        train_accuracy.append(accuracy(o, batch_label.int()))
-        
+      #permutation = torch.randperm(train_inputs.size(0))
+      
+      train_accuracy = []
+      train_loss = []
+      for i, data in enumerate(train_data):
+        inputs, labels = data
         optimizer.zero_grad()
+        o = model(inputs)
+        loss_val = loss(o, labels)
+        train_loss.append(loss_val)
+        train_accuracy.append(accuracy(o, labels))
+
         loss_val.backward()
         optimizer.step()
-
-        #Increase global step
-        global_step += 1
-
-      #Evaluate Model
-      valid_pred = model(valid_inputs)
-      valid_accuracy = accuracy(valid_pred, valid_labels.int())
-      
-      #train_logger.add_scalar('train/accuracy', np.mean(train_accuracy), global_step=global_step)
-      valid_logger.add_scalar('valid/accuracy', valid_accuracy, global_step=global_step)
-    
+        
     #Save Model
     save_model(model)
-    print(valid_accuracy)
+    #print(valid_accuracy)
     print(train_accuracy)
 
 
