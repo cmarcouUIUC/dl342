5ef013569ddc6b1187eda5c9f5374f335f4511d9
cmarcouUIUC <cmarcou@uillinois.edu>
2024-03-08 16:13:16 +0000

    train prog

--- homework2/homework/models.py
+++ homework2/homework/models.py
@@ -5,22 +5,41 @@ import torch.nn.functional as F
 class ClassificationLoss(torch.nn.Module):
     def forward(self, input, target):
         return F.cross_entropy(input,target)
-        
+
 
 class CNNClassifier(torch.nn.Module):
-    def __init__(self, layers=[32,64], n_input_channels=3, kernel_size=3):
+  class Block(torch.nn.Module):
+    def __init__(self, n_input, n_output, stride=1):
       super().__init__()
-      L=[]
-      c=n_input_channels
-      for l in layers:
-        L.append(torch.nn.Conv2d(c,l,kernel_size))
-        L.append(torch.nn.ReLU())
-        c=l
-      L.append(torch.nn.Conv2d(c,6, kernel_size=1))
-      self.network = torch.nn.Sequential(*L)
-
+      self.net = torch.nn.Sequential(
+        torch.nn.Conv2d(n_input, n_output, kernel_size=3, padding=1, stride=stride),
+        torch.nn.ReLU(),
+        torch.nn.Conv2d(n_output, n_output, kernel_size=3, padding=1),
+        torch.nn.ReLU()
+      )
+    
     def forward(self, x):
-       return self.network(x).mean(dim=[1,2,3])
+        return self.net(x)
+      
+  def __init__(self, layers=[32,64,128], n_input_channels=3):
+    super().__init__()
+    L = [torch.nn.Conv2d(n_input_channels, 32, kernel_size=7, padding=3, stride=2),
+        torch.nn.ReLU(),
+        torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1)]
+    c = 32
+    for l in layers:
+        L.append(self.Block(c, l, stride=2))
+        c = l
+    self.network = torch.nn.Sequential(*L)
+    self.classifier = torch.nn.Linear(c, 6)
+    
+  def forward(self, x):
+    # Compute the features
+    z = self.network(x)
+    # Global average pooling
+    z = z.mean(dim=[2,3])
+    # Classify
+    return self.classifier(z)
 
 
 def save_model(model):
